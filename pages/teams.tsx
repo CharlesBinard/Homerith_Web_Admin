5be1a06dd3b030808d9b2d2302c802852dfa4414
corvus <tashikomaa@gmail.com>
2019-11-23 19:37:43 +0100

    feat: add skelton page

---
+++ pages/teams.tsx
@@ -0,0 +1,28 @@
+import { useApolloClient } from '@apollo/react-hooks';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import InfoBanner from '../src/components/InfoBanner';
+import AddTeam from '../src/containers/AddTeam';
+
+const teamsPage: NextPage = () => {
+  const client = useApolloClient();
+
+  useEffect(() =>
+    client.writeData({
+      data: { activeRoute: { __typename: 'Route', name: 'teams', parentHref: null, parentAs: null } },
+    }),
+  );
+
+  return (
+    <>
+      <Head>
+        <title>Home | Homerith</title>
+      </Head>
+      <InfoBanner />
+      <AddTeam />
+    </>
+  );
+};
+
+export default teamsPage;
