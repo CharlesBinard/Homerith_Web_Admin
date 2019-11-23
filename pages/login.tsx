4f5fb2d3e6080a839d4ae2cdac47c2e92b047cbf
corvus <tashikomaa@gmail.com>
2019-11-23 19:36:29 +0100

    feat: add skelton page

---
+++ pages/login.tsx
@@ -0,0 +1,26 @@
+import { useApolloClient } from '@apollo/react-hooks';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import InfoBanner from '../src/components/InfoBanner';
+
+const loginPage: NextPage = () => {
+  const client = useApolloClient();
+
+  useEffect(() =>
+    client.writeData({
+      data: { activeRoute: { __typename: 'Route', name: 'Login', parentHref: null, parentAs: null } },
+    }),
+  );
+
+  return (
+    <>
+      <Head>
+        <title>Login | Homerith</title>
+      </Head>
+      <InfoBanner />
+    </>
+  );
+};
+
+export default loginPage;
